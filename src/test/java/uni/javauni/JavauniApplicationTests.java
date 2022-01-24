4e1d443efba2035d2b78c5221c5ccd96c8f571b4
element <[elementeyes1337@gmail.com]>
2022-01-24 20:08:20 +0200

    Initial commit

---
+++ src/test/java/uni/javauni/JavauniApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.javauni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavauniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
